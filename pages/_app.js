ca39ce93c177c69626bf17985aa1e490cf2d8a65
Park Hyung Seo <sonbbal20@gmail.com>
2021-01-04 18:37:53 +0900

    Add Styled-Comoponents

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  li {
+      list-style: none;
+  }
+`
+
+const theme = {
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
